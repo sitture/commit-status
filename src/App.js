4670b8a6380737f8f8b5303a7d275ab5dc0c5a2f
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-07-20 21:11:56 +0100

    Adds the header components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  render() {
+    return(
+      <div className="App" >
+        <Helmet
+          htmlAttributes={{ lang: "en", amp: undefined }} // amp takes no value
+          meta={[
+            { name: "description", content: "A simple React app that shows a list of projects with their Github commit status and use it as a dashboard to view status of your CI pipelines." }
+          ]}
+          title="CommitStatus"
+        />
+        <div className="App-header">
+          <Title />
+        </div>
+      </div>
+    );
+  }
+};
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Title = () => <span>CommitStatus</span>
+
+export default Title;
