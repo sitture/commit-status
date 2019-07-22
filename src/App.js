92060c89f0491614794977d707e1e020c3c9aa1c
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-07-22 13:46:01 +0100

    Adds circle ci config

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import './App.css';
 import Header from './components/Header/Header';
-import ProjectList from './components/ProjectList/ProjectList'
+import ProjectList from './components/ProjectList/ProjectList';
 
 export default class App extends Component {
   render() {
-    return(
-      <div className="App" >
+    return (
+      <div className="App">
         <Helmet
-          htmlAttributes={{ lang: "en", amp: undefined }}
+          htmlAttributes={{ lang: 'en', amp: undefined }}
           meta={[
-            { name: "description", content: "A simple React app that shows a list of projects with their Github commit status and use it as a dashboard to view status of your CI pipelines." }
+            {
+              name: 'description',
+              content:
+                'A simple React app that shows a list of projects with their Github commit status and use it as a dashboard to view status of your CI pipelines.',
+            },
           ]}
           title="CommitStatus"
         />
@@ -20,4 +24,4 @@ export default class App extends Component {
       </div>
     );
   }
-};
+}
--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,69 +1,79 @@
 import React from 'react';
-import './ProjectList.css'
+import './ProjectList.css';
 
 var axios = require('axios');
 
 export default class ProjectList extends React.Component {
+  state = {
+    projects: [],
+  };
 
-    state = {
-        projects: []
-    };
+  componentDidMount() {
+    var th = this;
+    var processedProjects = [];
+    this.serverRequest = axios.get(this.props.source).then(function(result) {
+      var status = result.data.defaultStatus;
+      result.data.projects.map(function(project) {
+        processedProjects.push({ name: project, status: status });
+      });
+      th.setState({
+        projects: processedProjects,
+      });
+      const params = {};
+      if (process.env) {
+        params.headers = {
+          Authorization: process.env.REACT_APP_GITHUB_TOKEN,
+        };
+      }
+      console.log(params);
+      let promiseArray = processedProjects.map(project =>
+        axios.get(
+          `https://api.github.com/repos/${project.name}/commits/master/status`,
+          params
+        )
+      );
+      Promise.all(promiseArray)
+        .then(
+          results => {
+            console.log('values', results);
+            processedProjects = [];
+            results.map(function(project) {
+              processedProjects.push({
+                name: project.data.repository.full_name,
+                status: project.data.state,
+              });
+            });
+            th.setState({
+              projects: processedProjects,
+            });
+          },
+          reason => {
+            console.log('error', reason);
+          }
+        )
+        .catch(console.log());
+    });
+  }
 
-    componentDidMount() {
-        var th = this;
-        var processedProjects = []
-        this.serverRequest =
-            axios.get(this.props.source)
-                .then(function(result) {
-                    var status = result.data.defaultStatus
-                    result.data.projects.map(function(project) {
-                        processedProjects.push({"name": project, "status": status})
-                    })
-                    th.setState({
-                        projects: processedProjects
-                    });
-                    const params = { }
-                    if (process.env) {
-                        params.headers = {
-                            Authorization: process.env.REACT_APP_GITHUB_TOKEN
-                        }
-                    }
-                    console.log(params)
-                    let promiseArray = processedProjects.map(project => axios.get(`https://api.github.com/repos/${project.name}/commits/master/status`, params));
-                    Promise.all(promiseArray)
-                        .then(
-                            results => {
-                                console.log('values', results)
-                                processedProjects = []
-                                results.map(function (project) {
-                                    processedProjects.push({ "name": project.data.repository.full_name, "status": project.data.state })
-                                })
-                                th.setState({
-                                    projects: processedProjects
-                                });
-                            },
-                            reason => {
-                                console.log('error', reason)
-                            });
-                });
-    }
-
-    componentWillUnmount() {
-        this.serverRequest.abort();
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.projectStatus}
-                {this.state.projects.map(function(project, index) {
-                    return (
-                        <div key={index} className="project">
-                            <a target="_blank" rel="noopener noreferrer" href={`https://github.com/${project.name}`} >{project.name}</a> - {project.status}
-                        </div>
-                    );
-                })}
+  render() {
+    return (
+      <div>
+        {this.state.projectStatus}
+        {this.state.projects.map(function(project, index) {
+          return (
+            <div key={index} className="project">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://github.com/${project.name}`}
+              >
+                {project.name}
+              </a>{' '}
+              - {project.status}
             </div>
-        );
-    }
-};
+          );
+        })}
+      </div>
+    );
+  }
+}
