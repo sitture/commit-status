b1cf892d919ab875e9255872e7c323ca9e2f636c
Feliks Toomsoo <feliks.toomsoo@gmail.com>
2019-10-09 19:38:58 +0300

    Add localstorage support

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -7,118 +7,158 @@ import ProjectDetails from '../ProjectDetails/ProjectDetails';
 var axios = require('axios');
 
 export default class ProjectList extends React.Component {
+  DEFAULT_STATUS = 'pending';
+  LS_PROJECTS_KEY = 'projects';
+
   state = {
     projects: [],
   };
 
-  componentDidMount =() => {
-    let th = this;
-    let processedProjects = [];
-    this.serverRequest = axios.get(this.props.source).then(function(result) {
-      var status = result.data.defaultStatus;
-      result.data.projects.map(function(project) {
-        return processedProjects.push({ name: project, status: status });
-      });
-      th.setState({
-        projects: processedProjects.sort((a, b) => {
-          if (a.name < b.name) 
-            return -1
-          else if (a.name > b.name)
-            return 1
-          return a.status > b.status ? -1 : 1
-        }),
-      });
-      const params = {};
-      if (process.env.REACT_APP_GITHUB_TOKEN) {
-        params.headers = {
-          Authorization: process.env.REACT_APP_GITHUB_TOKEN,
-        };
-      }
-    
-      let promiseArray = processedProjects.map(project =>{
-        return(
-        axios.get(
-          `https://api.github.com/repos/${project.name}/commits/master/status`,
-          params
-        )
-        )
-      }
+  componentDidMount = () => {
+    this.loadProjects();
+  };
+
+  loadProjects = () => {
+    const projects = this.getProjectsFromLocalStorage();
+
+    const sortedProjects = projects
+      .map((project) => {
+        return {name: project, status: this.DEFAULT_STATUS};
+      })
+      .sort(this.sortProjects);
+
+    this.setState({projects: sortedProjects}, this.loadProjectStatuses);
+  };
+
+  loadProjectStatuses = () => {
+    const params = {};
+
+    if (process.env.REACT_APP_GITHUB_TOKEN) {
+      params.headers = {
+        Authorization: process.env.REACT_APP_GITHUB_TOKEN,
+      };
+    }
+
+    let promiseArray = this.state.projects.map((project) => {
+      return axios.get(
+        `https://api.github.com/repos/${project.name}/commits/master/status`,
+        params,
       );
-      Promise.all(promiseArray)
-        .then(
-          results => {
-            console.log('values', results);
-            processedProjects = [];
-            results.map(function(project) {
-              return processedProjects.push({
+    });
+    Promise.all(promiseArray)
+      .then(
+        (results) => {
+          const sortedProjects = results
+            .map(function(project) {
+              return {
                 name: project.data.repository.full_name,
                 status: project.data.state,
-              });
-            });
-            th.setState({
-              projects: processedProjects,
-            });
-          },
-          reason => {
-            console.log('error', reason);
-          }
-        )
-        .catch(console.log());
-    });
-  }
+              };
+            })
+            .sort(this.sortProjects);
 
+          this.setState({
+            projects: sortedProjects,
+          });
+        },
+        (reason) => {
+          console.log('error', reason);
+        },
+      )
+      .catch(console.log());
+  };
+
+  sortProjects = (a, b) => {
+    if (a.name < b.name) return -1;
+    else if (a.name > b.name) return 1;
+    return a.status > b.status ? -1 : 1;
+  };
 
   handlePress = (name) => {
-    
-    let filteredArray = this.state.projects.filter((project)=>project.name!==name)
-    
+    let filteredArray = this.state.projects.filter(
+      (project) => project.name !== name,
+    );
+
+    this.removeProjectFromLocalStorage(name);
+
     this.setState({
-      projects : filteredArray
-    })
-  }
+      projects: filteredArray,
+    });
+  };
 
   addProject = (project) => {
     let flag = 0;
-    this.state.projects.forEach(proj=>{
-      if(proj.name===project.name&&proj.status===project.status){
-        flag =1;
-    
+    this.state.projects.forEach((proj) => {
+      if (proj.name === project.name && proj.status === project.status) {
+        flag = 1;
       }
-    })
+    });
 
-    if(!(flag===1)){
-    let newArray = [ ...this.state.projects, project ];
-   
-    this.setState({ projects : newArray })
+    if (!(flag === 1)) {
+      let newArray = [...this.state.projects, project].sort(this.sortProjects);
+      this.addProjectToLocalStorage(project.name);
+
+      this.setState({projects: newArray});
     }
+  };
+
+  getProjectsFromLocalStorage() {
+    const projects = localStorage.getItem(this.LS_PROJECTS_KEY);
+
+    if (!projects) {
+      return [];
+    }
+
+    return JSON.parse(projects);
   }
 
-  handleProjectClick(index){
+  addProjectToLocalStorage = (projectName) => {
+    const projects = this.getProjectsFromLocalStorage();
+    projects.push(projectName);
+
+    localStorage.setItem(this.LS_PROJECTS_KEY, JSON.stringify(projects));
+  };
+
+  removeProjectFromLocalStorage = (projectName) => {
+    const projects = this.getProjectsFromLocalStorage();
+    const updatedProjectsList = projects.filter(
+      (project) => project !== projectName,
+    );
+
+    localStorage.setItem(
+      this.LS_PROJECTS_KEY,
+      JSON.stringify(updatedProjectsList),
+    );
+  };
+
+  handleProjectClick(index) {
     this.setState({
       projects: this.state.projects.map((project, pIndex) => {
-        if (pIndex !== index || (pIndex === index && project.isOpen)){
-          project.isOpen = false
-          return project
+        if (pIndex !== index || (pIndex === index && project.isOpen)) {
+          project.isOpen = false;
+          return project;
         }
-        project.isOpen = true
-        return project
-      })
-    })
+        project.isOpen = true;
+        return project;
+      }),
+    });
   }
 
   render = () => {
-   let handlePress = this.handlePress;
-   let addProject = this.addProject;
+    let handlePress = this.handlePress;
+    let addProject = this.addProject;
 
     return (
       <div>
-        <Search addProject = {(project)=>addProject(project)}/>
+        <Search addProject={(project) => addProject(project)} />
         {this.state.projectStatus}
         {this.state.projects.map((project, index) => {
           return (
-            <div key={index} className={`project ${project.status}`} 
+            <div
+              key={index}
+              className={`project ${project.status}`}
               onClick={() => this.handleProjectClick(index)}
-              >
+            >
               <a
                 target="_blank"
                 rel="noopener noreferrer"
@@ -127,19 +167,17 @@ export default class ProjectList extends React.Component {
                 {project.name}
               </a>{' '}
               - <span className={project.status}>{project.status}</span>
-
-              <button className="remove"  onClick={(index)=>handlePress(project.name)}>
+              <button
+                className="remove"
+                onClick={(index) => handlePress(project.name)}
+              >
                 Remove
-              </button>  
-
-              {
-                project.isOpen && <ProjectDetails name={project.name}/>
-              }
-
+              </button>
+              {project.isOpen && <ProjectDetails name={project.name} />}
             </div>
           );
         })}
       </div>
     );
-  }
+  };
 }
