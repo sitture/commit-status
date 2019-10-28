d5b4096d5b377cd62cac68299f323f9ee08819e5
Ondřej Merkun <ondrej.merkun@gmail.com>
2019-10-28 20:59:04 +0100

    Implement auto-refresh of the project statuses (#42)
    https://github.com/sitture/commit-status/issues/41

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import "./ProjectDetails.css";
 var axios = require('axios');
+
+const params = {};
+
+if (process.env.REACT_APP_GITHUB_TOKEN) {
+  params.headers = {
+    Authorization: process.env.REACT_APP_GITHUB_TOKEN,
+  };
+}
+
 export default class ProjectDetails extends Component {
     state={
-        commitDetails:null
+        commitDetails:null,
+        shouldRefresh: false,
     }
     componentDidMount=()=>{
-        const params = {};
-      if (process.env.REACT_APP_GITHUB_TOKEN) {
-        params.headers = {
-          Authorization: process.env.REACT_APP_GITHUB_TOKEN,
-        };
-      }
-  
       axios.get(
         `https://api.github.com/repos/${this.props.name}/commits`,
         params
       ).then(data=>this.setState({commitDetails:data.data}))
     }
+
+    componentWillUnmount () {
+      clearInterval(this.interval);
+    }
+ 
+    toggleAutoRefresh (e) {
+      e.stopPropagation(); // Prevent the click event from being fired on the parent elements as well
+
+      this.setState({
+        shouldRefresh: !this.state.shouldRefresh,
+      }, () => {
+
+        clearInterval(this.interval);
+
+        if (this.state.shouldRefresh) {
+          this.interval = setInterval(() => {
+            axios.get(
+              `https://api.github.com/repos/${this.props.name}/commits`,
+              params
+            )
+            .then(data => this.setState({ commitDetails:data.data }))
+          }, 30000); // Refresh every 30 seconds
+        }
+
+      })
+    }
+
     render() {
        if(!this.state.commitDetails){
            return(
@@ -25,7 +55,12 @@ export default class ProjectDetails extends Component {
            )
        }
         return (
-            <div>
+            <div className='ProjectDetails'>
+
+              <button onClick={(e) => this.toggleAutoRefresh(e)} className='autoRefreshButton'>
+                Toggle Auto Refresh {this.state.shouldRefresh ? 'OFF' : 'ON'}
+              </button>
+
              Commit Details
              <table>
                  <thead>
